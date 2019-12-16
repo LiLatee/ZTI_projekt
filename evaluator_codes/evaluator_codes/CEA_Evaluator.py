15307d8ebdc51e7d2965348c0def9fa1bf8fc07b
ngiersz <ngiersz@gmail.com>
2019-12-16 22:22:03 +0100

    With translation - 59%

--- evaluator_codes/evaluator_codes/CEA_Evaluator.py
+++ evaluator_codes/evaluator_codes/CEA_Evaluator.py
@@ -72,9 +72,9 @@ if __name__ == "__main__":
     # Lets assume the the ground_truth is a CSV file
     # and is present at data/ground_truth.csv
     # and a sample submission is present at data/sample_submission.csv
-    answer_file_path = "data/CEA_Round1_gt.csv"
+    answer_file_path = "/home/ngiersz/Projects/zti/ZTI_projekt/evaluator_codes/evaluator_codes/data/CEA_Round1_gt.csv"
     _client_payload = {}
-    _client_payload["submission_file_path"] = "data/my_submission3.csv"
+    _client_payload["submission_file_path"] = "/home/ngiersz/Projects/zti/ZTI_projekt/evaluator_codes/evaluator_codes/data/my_submission3.csv"
     _client_payload["aicrowd_submission_id"] = 1123
     _client_payload["aicrowd_participant_id"] = 1234
     
